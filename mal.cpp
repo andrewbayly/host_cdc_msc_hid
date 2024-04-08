e56de3c273eaf73e13c62058d2f51a3d3c4701e1
Andrew Bayly <andrew.bayly@mac.com>
2024-04-07 22:51:01 -0700

    added exclamation marks in salutation

--- mal.cpp
+++ mal.cpp
@@ -676,7 +676,7 @@ void mal_key_pressed(char ch){
 }
 
 void mal_init(){ 
-  std::string s("*** Welcome to Lisp! ***\r > "); 
+  std::string s("*** Welcome to Lisp!!! ***\r > "); 
   print_string(s); 
 
 //  string s2("(((ppp)))"); 
